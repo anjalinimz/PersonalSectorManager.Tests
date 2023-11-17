89839c67362de1772be4aa4662a2f3f85a04a828
anjalinimz <anjalinimz@gmail.com>
2023-11-17 02:31:42 +0200

    initialCommit

---
+++ Services/SectorServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PersonalSectorManager.Models;
+using PersonalSectorManager.Service;
+
+namespace PersonalSectorManager.Tests.Services
+{
+	public class SectorServiceTest
+	{
+        [Fact]
+        public void RetrieveHierarchicalSectors_ShouldReturnHierarchicalList()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ProfileDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase02")
+                .Options;
+
+            using (var context = new ProfileDBContext(options))
+            {
+                // Populate the in-memory database with test data
+                context.Sectors.Add(new Sector("Parent1") { SectorId = 1, ParentId = null });
+                context.Sectors.Add(new Sector("Parent1-Child1") { SectorId = 2, ParentId = 1 });
+                context.Sectors.Add(new Sector("Parent1-Child2") { SectorId = 3, ParentId = 1 });
+                context.Sectors.Add(new Sector("Parent1-Child2-Child1") { SectorId = 4, ParentId = 3 });
+                context.Sectors.Add(new Sector("Parent2") { SectorId = 5, ParentId = null });
+                context.SaveChanges();
+            }
+
+            using (var context = new ProfileDBContext(options))
+            {
+                var mockLogger = new Mock<ILogger<SectorService>>();
+                var sectorService = new SectorService(context, mockLogger.Object);
+
+                // Act
+                var hierarchicalList = sectorService.RetrieveHierarchicalSectors();
+
+                // Assert
+                Assert.NotNull(hierarchicalList);
+                Assert.Equal(5, hierarchicalList.Count);
+
+                Assert.Equal("Parent1", hierarchicalList[0].Name);
+                Assert.Equal(0, hierarchicalList[0].Level);
+                Assert.True(hierarchicalList[0].Disabled);
+
+                Assert.Equal("Parent1-Child1", hierarchicalList[1].Name);
+                Assert.Equal(1, hierarchicalList[1].Level);
+                Assert.False(hierarchicalList[1].Disabled);
+
+                Assert.Equal("Parent1-Child2", hierarchicalList[2].Name);
+                Assert.Equal(1, hierarchicalList[2].Level);
+                Assert.True(hierarchicalList[2].Disabled);
+
+                Assert.Equal("Parent1-Child2-Child1", hierarchicalList[3].Name);
+                Assert.Equal(2, hierarchicalList[3].Level);
+                Assert.False(hierarchicalList[3].Disabled);
+
+                Assert.Equal("Parent2", hierarchicalList[4].Name);
+                Assert.Equal(0, hierarchicalList[4].Level);
+                Assert.False(hierarchicalList[4].Disabled);
+            }
+        }
+    }
+}
+
---
+++ Services/UserServiceTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PersonalSectorManager.Models;
+using PersonalSectorManager.Service;
+using PersonalSectorManager.ViewModels;
+
+namespace PersonalSectorManager.Tests.Services
+{
+	public class UserServiceTest
+	{
+        private readonly ProfileDBContext _dbContext;
+        private readonly UserService _userService;
+
+
+        public UserServiceTest()
+        {
+
+            var mockLogger = new Mock<ILogger<UserService>>();
+            var options = new DbContextOptionsBuilder<ProfileDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase01")
+                .Options;
+
+            _dbContext = new ProfileDBContext(options);
+            _userService = new UserService(_dbContext, mockLogger.Object);
+
+
+        }
+
+
+        [Fact]
+        public void GetUser_WithValidUserId_ShouldReturnUserViewModel()
+        {
+            // Arrange
+            var userId = 10;
+
+            //Saving Sectors to in-memory DB
+            _dbContext.Sectors.AddRange(new Sector("Sector 1") { SectorId = 11 },
+                new Sector("Sector 2") { SectorId = 22 });
+            _dbContext.SaveChanges();
+
+            // Saving User to in-memory DB
+            var user = new User("Anjali Nimesha")
+            {
+                UserId = userId,
+                AgreeToTerms = true,
+                UserSectors = new List<UserSector>()
+                {
+                    new UserSector(userId, 11),
+                    new UserSector(userId, 22)
+                }
+            };
+
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            // Act
+            var userViewModel = _userService.GetUser(userId);
+
+            // Assert
+            Assert.NotNull(userViewModel);
+            Assert.Equal(userId, userViewModel.UserId);
+            Assert.Equal("Anjali Nimesha", userViewModel.UserName);
+            Assert.True(userViewModel.AgreeToTerms);
+            Assert.NotNull(userViewModel.Sectors);
+            Assert.Equal(2, userViewModel.Sectors.Count);
+            Assert.Equal(11, userViewModel.Sectors[0].Value);
+            Assert.Equal("Sector 1", userViewModel.Sectors[0].Name);
+            Assert.Equal(22, userViewModel.Sectors[1].Value);
+            Assert.Equal("Sector 2", userViewModel.Sectors[1].Name);
+        }
+
+        [Fact]
+        public void GetUser_WithInvalidUserId_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var userId = -1;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => _userService.GetUser(userId));
+        }
+
+        [Fact]
+        public void SaveUser_WithValidUserViewModel_ShouldSaveUserToDatabase()
+        {
+            // Arrange
+            //Saving Sectors to in-memory DB
+            _dbContext.Sectors.AddRange(new Sector("Sector 1") { SectorId = 111 },
+                new Sector("Sector 2") { SectorId = 222 });
+            _dbContext.SaveChanges();
+
+            var userViewModel = new UserViewModel
+            {
+                UserId = 0,
+                UserName = "John Doe",
+                AgreeToTerms = true,
+                SelectedSectorIds = new List<int> { 111, 222 }
+            };
+
+            // Act
+            var userId = _userService.SaveUser(userViewModel);
+
+            // Assert
+            Assert.NotEqual(0, userId); // User ID should be assigned after saving
+            Assert.Equal(userViewModel.UserName, _dbContext.Users.Single(u => u.UserId == userId).UserName);
+            Assert.Equal(2, _dbContext.UserSectors.Where(us => us.UserId == userId).ToList().Count);
+        }
+
+        [Fact]
+        public void SaveUser_WithInValidUserViewModel_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var userViewModel = new UserViewModel
+            {
+                UserId = 0,
+                UserName = "John Doe",
+                AgreeToTerms = true,
+                SelectedSectorIds = new List<int>() // Empty Selected sectors
+            };
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => _userService.SaveUser(userViewModel));
+        }
+    }
+}
+
